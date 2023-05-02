34146748ae7e21b4a2bc51a799ee9fde7a668185
Eakudompong Chanoknan <ceguantz96@outlook.com>
2023-05-02 11:39:40 +0900

    update

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -3,17 +3,19 @@ import React from "react";
 function ProjectItem(props) {
   const { title, imgUrl, stack, link } = props;
   return (
-    <div className='border border-stone-900 rounded-sm overflow-hidden '>
+    <div className="border border-stone-900 rounded-sm overflow-hidden ">
       <img
         src={imgUrl}
-        alt='Project'
-        className='w-full h-36 md:h-48  object-cover cursor-pointer'
+        alt="Project"
+        className="w-full h-36 md:h-48  object-cover cursor-pointer"
       />
-      <div className='w-full p-4'>
-        <h3 className='text-lg md:text-xl mb-2 md:mb-3 font-semibold'>{title}</h3>
-        <p className='flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm'>
+      <div className="w-full p-4">
+        <h3 className="text-lg md:text-xl mb-2 md:mb-3 font-semibold">
+          {title}
+        </h3>
+        <p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm">
           {stack.map((item) => (
-            <span className='inline-block px-2 py-1 font-semibold border border-stone-900 rounded-md'>
+            <span className="inline-block px-2 py-1 font-semibold border border-stone-900 rounded-md">
               {item}
             </span>
           ))}
