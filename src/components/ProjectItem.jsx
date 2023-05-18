1c630cec2095611f9042321d9daacbe280ba3d50
Eakudompong Chanoknan <ceguantz96@outlook.com>
2023-05-18 11:03:48 +0900

    darkmode todo localization

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -15,7 +15,7 @@ function ProjectItem(props) {
         </h3>
         <p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm">
           {stack.map((item) => (
-            <span className="inline-block px-2 py-1 font-semibold border border-stone-900 rounded-md">
+            <span className="inline-block px-2 py-1 font-semibold border border-stone-900 rounded-md dark:border-white">
               {item}
             </span>
           ))}
