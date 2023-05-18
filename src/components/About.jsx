a1b41be2436e2965e28625d6a47a8a98d65d3312
Eakudompong Chanoknan <ceguantz96@outlook.com>
2023-05-18 11:33:58 +0900

    fix text in about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,19 +2,21 @@ import React from "react";
 
 function About() {
   return (
-    <div className='flex items-center justify-center flex-col text-center pt-20 pb-6'>
-      <h1 className='text-3xl md:text-4xl mb-1 md:mb-3 font-bold'>Eakudompong Chanoknan</h1>
-      <h2 className='text-base md:text-2xl  mb-3 font-medium'>
+    <div className="flex items-center justify-center flex-col text-center text-justify pt-20 pb-6">
+      <h1 className="text-3xl md:text-4xl mb-1 md:mb-3 font-bold">
+        Eakudompong Chanoknan
+      </h1>
+      <h2 className="text-base md:text-2xl  mb-3 font-medium">
         Software Engineer & Full-stack Web Developer
       </h2>
-      <p className='text-lg max-w-xl mb-6 font-bold '>
-        Hi!, I'm Chanoknan, i'm a Full-stack developer with a passion for learning and exploring new
-        technologies., I have been building my skills through coursework and personal projects, and
-        I'm always eager to take on new challenges.I'm particularly interested in Web
-        Technologies,Dev/Ops and native mobile app development. <br />
-        I'm committed to writing clean, maintainable code and staying up to date with industry
-        trends. I'm excited to learn and grow, and I'm always looking for opportunities to apply my
-        skills.
+      <p className="text-lg max-w-3xl mb-6 font-bold ">
+        Hi!, I'm Chanoknan, i'm a Full-stack developer with a passion for
+        learning and exploring new technologies. I have been building my skills
+        through coursework and personal projects, and I'm always eager to take
+        on new challenges.I'm particularly interested in Web development Dev/Ops
+        and Mobile app Development I'm committed to writing clean, maintainable
+        code and staying up to date with industry trends. I'm excited to learn
+        and grow, and I'm always looking for opportunities to apply my skills.
       </p>
     </div>
   );
