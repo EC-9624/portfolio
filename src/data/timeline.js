a7afb8ff3e7d976ce37f5b4e0884b35131ddd126
Eakudompong Chanoknan <ceguantz96@outlook.com>
2023-06-06 08:13:11 +0900

    todo update picture

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -1,36 +1,28 @@
 export default [
   {
     year: "Today",
-    title: "YouTube Content Creator",
-    duration: "6 years",
-    details:
-      "My humble attempt at building my version of the digital world one step at a time by documenting my coding journey and creating software engineering and computer science content to help those coming behind me.",
+    title: "ECC Computer college",
+    duration: "2 years",
+    details: "",
   },
   {
-    year: "Today",
-    title: "Solo Indie Developer",
-    duration: "4 years",
-    details:
-      "I'm a solo indie developer building software for myself and clients to showcase on YouTube. All coding projects are built from the ground up, from planning and designing all the way to solving real-life problems with code.",
-  },
-  {
-    year: "2019",
+    year: "2022",
     title: "Software Engineer",
     duration: "2 years",
-    details:
-      "I helped build an enterprise application for a Fortune 500 company in an Agile team setting. Our tech stack consisted of TypeSciprt, Angular, Java, Gradle, and a handful of other languages and tools.",
-  },
-  {
-    year: "2018",
-    title: "Bachelor's Degree in Computer Science",
-    duration: "5 years",
-    details:
-      "I pursued the knowledge that a computer science degree had to offer. I learned about computer architecture, computer science theory, an array of programming, and aspects of artificial intelligence.",
+    details: "",
   },
   {
     year: "2020",
     title: "First Line of Code",
-    duration: "the beginning",
-    details: "I start writing code during the pandamic.",
+    duration: "",
+    details:
+      "I wrote my first line of code during my time at the University. Despite my interest in International Relations, I realized that I didn't want to pursue a career in that field. Instead, I started coding and haven't stopped ever since.",
+  },
+  {
+    year: "2020",
+    title: "Bachelor's Degree in International Relations",
+    duration: "3.5 years",
+    details:
+      "I graduated from Srinakharinwirot University in Thailand, where I pursued a degree in International Relations. with an interest in global politics,diplomacy and global economy. ",
   },
 ];
