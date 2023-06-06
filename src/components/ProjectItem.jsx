d51101612c6370aec09a019b007002c8dbd285d4
Eakudompong Chanoknan <ceguantz96@outlook.com>
2023-06-06 15:25:51 +0900

    add Details jp

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -12,7 +12,9 @@ function ProjectItem(props) {
       />
       <div className="w-full p-4">
         <h3 className="text-lg md:text-xl mb-2 md:mb-3 font-semibold">
-          <a href={link}> {title}</a>
+          <a href={link} target="_blank">
+            {title}
+          </a>
         </h3>
 
         <p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm">
--- src/data/timeline.js
+++ src/data/timeline.js
@@ -5,6 +5,8 @@ export default [
     duration: "2 years",
     details:
       "I decided to enroll in ECC Computer College to kickstart  my career as a software Developer in japan I learned about Programing,System Design,Networking and get my hand dirty with group and personal projects. ",
+    details_jp:
+      "日本でソフトウェアエンジニアとして就職するため、ECCコンピューター専門学校に入学することを決めました。プログラミング、システムデザイン、ネットワーキングについて学び、グループや個人のプロジェクトで実践的な経験を積みました。",
   },
   {
     year: "2021",
@@ -12,6 +14,8 @@ export default [
     duration: "1 years",
     details:
       "Unfortunately, I graduated in the midst of the pandemic with a lot of free time. I was looking for my options for the future, and luckily, I found scholarships to study Japanese at Asahikawa Welfare College in Hokkaido. This marks the start of my journey in Japan.",
+    details_jp:
+      "残念ながら、私はコロナの最中に卒業しました。将来の選択肢を探していたところ、幸運にも北海道の旭川福祉専門学校で日本語の奨学金を見つけることができました。これが私の日本での旅の始まりとなります。",
   },
 
   {
@@ -20,6 +24,8 @@ export default [
     duration: "3.5 years",
     details:
       "I graduated from Srinakharinwirot University in Thailand, where I pursued a degree in International Relations. with an interest in global politics,diplomacy and global economy. ",
+    details_jp:
+      "タイのスリーナカリンウィロート大学を卒業しました。国際関係学の学位を追求し、グローバル政治、外交、そしてグローバル経済に関心を持っていました。",
   },
   {
     year: "2019",
@@ -27,5 +33,7 @@ export default [
     duration: "The begining",
     details:
       "I wrote my first line of code during my time at the University. Despite my interest in International Relations, I realized that I didn't want to pursue a career in that field. Instead, I started coding and haven't stopped ever since.",
+    details_jp:
+      "大学時代に最初のコードを書きました。国際関係に興味があったにもかかわらず、その分野でのキャリアを追求したくないと感じました。代わりにコーディングを始め、それ以来止めることはありませんでした。",
   },
 ];
