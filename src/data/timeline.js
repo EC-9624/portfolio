89084684416f90f7013b0abf2c2e74611b998ee0
Eakudompong Chanoknan <ceguantz96@outlook.com>
2023-06-06 09:20:52 +0900

    update timeline todo thumbnails

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -1,28 +1,31 @@
 export default [
   {
     year: "Today",
-    title: "ECC Computer college",
+    title: "ECC Computer College",
     duration: "2 years",
-    details: "",
-  },
-  {
-    year: "2022",
-    title: "Software Engineer",
-    duration: "2 years",
-    details: "",
-  },
-  {
-    year: "2020",
-    title: "First Line of Code",
-    duration: "",
     details:
-      "I wrote my first line of code during my time at the University. Despite my interest in International Relations, I realized that I didn't want to pursue a career in that field. Instead, I started coding and haven't stopped ever since.",
+      "I decided to enroll in ECC Computer College to start my career as a software Developer in japan I learned about Programing,System Design,Networking and get my hand dirty with Group/Personal Projects.",
   },
   {
-    year: "2020",
+    year: "2021",
+    title: "First time in japan",
+    duration: "1 years",
+    details:
+      "Unfortunately, I graduated in the midst of the pandemic with a lot of free time. I was looking for my options for the future, and luckily, I found scholarships to study Japanese at Asahikawa Welfare College in Hokkaido. This marks the start of my journey in Japan.",
+  },
+
+  {
+    year: "2019",
     title: "Bachelor's Degree in International Relations",
     duration: "3.5 years",
     details:
       "I graduated from Srinakharinwirot University in Thailand, where I pursued a degree in International Relations. with an interest in global politics,diplomacy and global economy. ",
+  },
+  {
+    year: "2019",
+    title: "First Line of Code",
+    duration: "The begining",
+    details:
+      "I wrote my first line of code during my time at the University. Despite my interest in International Relations, I realized that I didn't want to pursue a career in that field. Instead, I started coding and haven't stopped ever since.",
   },
 ];
