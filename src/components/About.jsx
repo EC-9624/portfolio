e853de1cba3ad06fd4492dd39b328462a5fc7972
Eakudompong Chanoknan <ceguantz96@outlook.com>
2023-06-07 12:26:35 +0900

    todo add about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,13 +16,21 @@ function About() {
   const text = lang === "EN" ? text_EN : text_JP;
 
   return (
-    <div className='flex items-center justify-center flex-col text-center pt-20 pb-6 mx-4'>
-      <h1 className='text-2xl md:text-4xl mb-1 md:mb-1 font-bold'>Eakudompong Chanoknan</h1>
-      <h2 className='text-base md:text-lg  mb-1 font-medium'>エークドッムポン チャノックナン</h2>
-      <h2 className='text-base md:text-xl  mb-3 font-medium'>
+    <div className="flex items-center justify-center flex-col text-center pt-20 pb-6 mx-4">
+      <h1 className="text-2xl md:text-4xl mb-1 md:mb-1 font-bold">
+        Eakudompong Chanoknan
+      </h1>
+      <h2 className="text-base md:text-lg  mb-1 font-medium">
+        エークドッムポン チャノックナン
+      </h2>
+      <h2 className="text-base md:text-xl  mb-3 font-medium">
         Software Engineer & Full-stack Web Developer
       </h2>
-      <p className='text-sm max-w-xl mb-6 font-bold text-justify'>{text}</p>
+      <p className="text-sm max-w-xl mb-4 font-bold text-justify">{text}</p>
+      <div className="flex gap-2 ">
+        <p>Email: e.chanoknan96@outlook.com</p>
+        <p>Osaka,Japan</p>
+      </div>
     </div>
   );
 }
--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import timeline from "../data/timeline";
 import TimelineItem from "./TimelineItem";
 import Title from "./Title";
+import { LangContext } from "../App";
 
 function Timeline() {
+  const lang = React.useContext(LangContext);
   return (
     <div className="flex flex-col md:flex-row justify-center my-20 pl-2 mx-auto max-w-screen-xl">
       <div className="w-full md:w-7/12">
@@ -14,7 +16,7 @@ function Timeline() {
             year={item.year}
             title={item.title}
             duration={item.duration}
-            details={item.details}
+            details={lang === "EN" ? item.details : item.details_jp}
           />
         ))}
       </div>
